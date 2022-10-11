034ef782bcd7b01318d5b306fb993c3273b919df
Zainab Lawal <zainablaw1012@gmail.com>
2022-10-11 15:23:42 +0300

    Linked MongoDB Database

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const {connectToMongoDB} =  require('./db')
 
 const PORT = 7000;
 const app = express();
 
+connectToMongoDB()
 
 app.get('/', (req,res)=>{
     res.send('App is up and running')
 })
 
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
