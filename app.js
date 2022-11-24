7df17705d4ead80909bad1358a331d13b0da864c
Zainab Lawal <zainablaw1012@gmail.com>
2022-11-24 13:49:09 +0300

    Restructured API And created home endpoint

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const {connectToMongoDB} =  require('./db')
 const PORT = 7000;
 const app = express();
 
+// temporarily disabled to allow easy testing of home page
 connectToMongoDB()
 
+
 app.get('/', (req,res)=>{
-    res.send('App is up and running')
+    res.status(200).json({
+        status: "true",
+        message: "Welcome to Japa API"
+    })
 })
 
 app.use(function (err, req, res, next) {
@@ -16,6 +21,6 @@ app.use(function (err, req, res, next) {
     res.json({ error: err.message });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+
+
+module.exports = app
